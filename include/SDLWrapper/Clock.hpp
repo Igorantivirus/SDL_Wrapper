e114ae8f070b0e2bfcaf4dfa60ae781f5f4b0361
Игорь Антипов <igorantivirus@yandex.ru>
2026-01-23 18:58:56 +0700

    Добавлен метод паузы

--- include/SDLWrapper/Clock.hpp
+++ include/SDLWrapper/Clock.hpp
@@ -10,12 +10,17 @@ public:
     Clock();
 
     void start();
+    void pause(bool pause);
+
+    bool isPaused() const;
 
     std::uint64_t elapsedTimeMS() const;
     float elapsedTimeS() const;
 
 private:
-    std::uint64_t lastTimeMS_;
+    std::uint64_t lastTimeMS_ = 0;
+    std::uint64_t pauseStartMS_ = 0;
+    bool paused_ = false;
 };
 
 class ClockNS
@@ -24,6 +29,9 @@ public:
     ClockNS();
 
     void start();
+    void pause(bool pause);
+
+    bool isPaused() const;
 
     std::uint64_t elapsedTimeNS() const;
     float elapsedTimeMS() const;
@@ -31,6 +39,8 @@ public:
 
 private:
     std::uint64_t lastTimeNS_;
+    std::uint64_t pauseStartNS_ = 0;
+    bool paused_ = false;
 };
 
 } // namespace sdl3
--- src/Clock.cpp
+++ src/Clock.cpp
@@ -9,19 +9,46 @@ Clock::Clock()
 {
     start();
 }
-
 void Clock::start()
 {
     lastTimeMS_ = SDL_GetTicks();
+    paused_ = false;
+    pauseStartMS_ = 0;
+}
+void Clock::pause(bool pause)
+{
+    if (pause == paused_)
+        return;
+    if (pause)
+    {
+        pauseStartMS_ = SDL_GetTicks();
+    }
+    else
+    {
+        const std::uint64_t elapsed = SDL_GetTicks() - pauseStartMS_;
+        lastTimeMS_ += elapsed;
+    }
+    paused_ = pause;
+}
+bool Clock::isPaused() const
+{
+    return paused_;
 }
 std::uint64_t Clock::elapsedTimeMS() const
 {
+    if (paused_)
+        return pauseStartMS_ - lastTimeMS_;
     return SDL_GetTicks() - lastTimeMS_;
 }
 float Clock::elapsedTimeS() const
 {
-    return (SDL_GetTicks() - lastTimeMS_) / 1e3f;
+    return elapsedTimeMS() / 1e3f;
 }
+
+} // namespace sdl3
+
+namespace sdl3
+{
 
 ClockNS::ClockNS()
 {
@@ -30,17 +57,40 @@ ClockNS::ClockNS()
 void ClockNS::start()
 {
     lastTimeNS_ = SDL_GetTicksNS();
+    paused_ = false;
+    pauseStartNS_ = 0;
+}
+void ClockNS::pause(bool pause)
+{
+    if (pause == paused_)
+        return;
+    if (pause)
+    {
+        pauseStartNS_ = SDL_GetTicksNS();
+    }
+    else
+    {
+        const std::uint64_t elapsed = SDL_GetTicksNS() - pauseStartNS_;
+        lastTimeNS_ += elapsed;
+    }
+    paused_ = pause;
+}
+bool ClockNS::isPaused() const
+{
+    return paused_;
 }
 std::uint64_t ClockNS::elapsedTimeNS() const
 {
+    if (paused_)
+        return pauseStartNS_ - lastTimeNS_;
     return SDL_GetTicksNS() - lastTimeNS_;
 }
 float ClockNS::elapsedTimeMS() const
 {
-    return (SDL_GetTicksNS() - lastTimeNS_) / 1e6f;
+    return elapsedTimeNS() / 1e6f;
 }
 float ClockNS::elapsedTimeS() const
 {
-    return (SDL_GetTicksNS() - lastTimeNS_) / 1e9f;
+    return elapsedTimeNS() / 1e9f;
 }
 } // namespace sdl3
